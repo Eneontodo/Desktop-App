5eb9ec6326996261e0a0a26b0fc06c17ddc9ec7c
Eneontodo <149838860+Eneontodo@users.noreply.github.com>
2024-04-03 19:46:38 +0500

    Update Form1.Designer.cs

--- desktop/Form1.Designer.cs
+++ desktop/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Код, автоматически созданный конструктором форм Windows
 
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -81,9 +70,7 @@
             this.menuStrip1.SuspendLayout();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tabControl1
-            // 
+            
             this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
             this.tabControl1.Controls.Add(this.Home);
             this.tabControl1.Controls.Add(this.MStore);
@@ -98,9 +85,7 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1918, 977);
             this.tabControl1.TabIndex = 0;
-            // 
-            // Home
-            // 
+            
             this.Home.BackColor = System.Drawing.Color.Green;
             this.Home.Controls.Add(this.button11);
             this.Home.Controls.Add(this.label7);
@@ -109,9 +94,7 @@
             this.Home.Size = new System.Drawing.Size(1891, 969);
             this.Home.TabIndex = 5;
             this.Home.Text = "Home";
-            // 
-            // button11
-            // 
+
             this.button11.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button11.Image = ((System.Drawing.Image)(resources.GetObject("button11.Image")));
@@ -124,9 +107,7 @@
             this.button11.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button11.UseVisualStyleBackColor = true;
             this.button11.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // label7
-            // 
+
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label7.ForeColor = System.Drawing.SystemColors.Control;
@@ -135,9 +116,7 @@
             this.label7.Size = new System.Drawing.Size(637, 73);
             this.label7.TabIndex = 0;
             this.label7.Text = "Добро пожаловать!";
-            // 
-            // MStore
-            // 
+
             this.MStore.BackColor = System.Drawing.Color.OliveDrab;
             this.MStore.Controls.Add(this.button7);
             this.MStore.Controls.Add(this.start_MS);
@@ -148,9 +127,7 @@
             this.MStore.Size = new System.Drawing.Size(1891, 969);
             this.MStore.TabIndex = 0;
             this.MStore.Text = "Microsoft Store";
-            // 
-            // button7
-            // 
+
             this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
@@ -163,9 +140,7 @@
             this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // start_MS
-            // 
+
             this.start_MS.Cursor = System.Windows.Forms.Cursors.Hand;
             this.start_MS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.start_MS.Image = ((System.Drawing.Image)(resources.GetObject("start_MS.Image")));
@@ -175,17 +150,13 @@
             this.start_MS.TabIndex = 1;
             this.start_MS.UseVisualStyleBackColor = true;
             this.start_MS.Click += new System.EventHandler(this.start_MS_Click);
-            // 
-            // label3
-            // 
+
             this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
             this.label3.Location = new System.Drawing.Point(6, 3);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(800, 199);
             this.label3.TabIndex = 0;
-            // 
-            // VStudio
-            // 
+
             this.VStudio.BackColor = System.Drawing.Color.DarkViolet;
             this.VStudio.Controls.Add(this.button8);
             this.VStudio.Controls.Add(this.label4);
@@ -196,9 +167,7 @@
             this.VStudio.Size = new System.Drawing.Size(1891, 969);
             this.VStudio.TabIndex = 1;
             this.VStudio.Text = "Visual Studio";
-            // 
-            // button8
-            // 
+
             this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button8.Image = ((System.Drawing.Image)(resources.GetObject("button8.Image")));
@@ -211,17 +180,13 @@
             this.button8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // label4
-            // 
+
             this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
             this.label4.Location = new System.Drawing.Point(6, 3);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(802, 239);
             this.label4.TabIndex = 1;
-            // 
-            // button4
-            // 
+
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
@@ -231,9 +196,7 @@
             this.button4.TabIndex = 0;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // brows
-            // 
+
             this.brows.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(125)))), ((int)(((byte)(255)))));
             this.brows.Controls.Add(this.button12);
             this.brows.Controls.Add(this.label5);
@@ -243,9 +206,7 @@
             this.brows.Size = new System.Drawing.Size(1891, 969);
             this.brows.TabIndex = 3;
             this.brows.Text = "browser";
-            // 
-            // button12
-            // 
+
             this.button12.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button12.Image = ((System.Drawing.Image)(resources.GetObject("button12.Image")));
@@ -258,17 +219,13 @@
             this.button12.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button12.UseVisualStyleBackColor = true;
             this.button12.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // label5
-            // 
+
             this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
             this.label5.Location = new System.Drawing.Point(15, 12);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(802, 237);
             this.label5.TabIndex = 1;
-            // 
-            // button5
-            // 
+
             this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
@@ -278,9 +235,7 @@
             this.button5.TabIndex = 0;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // GrapEdit
-            // 
+
             this.GrapEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(64)))), ((int)(((byte)(59)))));
             this.GrapEdit.Controls.Add(this.button9);
             this.GrapEdit.Controls.Add(this.button6);
@@ -290,9 +245,7 @@
             this.GrapEdit.Size = new System.Drawing.Size(1891, 969);
             this.GrapEdit.TabIndex = 4;
             this.GrapEdit.Text = "Graphic Editor";
-            // 
-            // button9
-            // 
+
             this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button9.Image = ((System.Drawing.Image)(resources.GetObject("button9.Image")));
@@ -305,9 +258,7 @@
             this.button9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button6
-            // 
+
             this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
@@ -317,17 +268,13 @@
             this.button6.TabIndex = 1;
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // label6
-            // 
+
             this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
             this.label6.Location = new System.Drawing.Point(3, 10);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(793, 249);
             this.label6.TabIndex = 0;
-            // 
-            // Xternus
-            // 
+
             this.Xternus.BackColor = System.Drawing.SystemColors.Desktop;
             this.Xternus.Controls.Add(this.Xternu);
             this.Xternus.Controls.Add(this.label2);
@@ -339,25 +286,19 @@
             this.Xternus.Size = new System.Drawing.Size(1891, 969);
             this.Xternus.TabIndex = 2;
             this.Xternus.Text = "Xternu";
-            // 
-            // Xternu
-            // 
+
             this.Xternu.Controls.Add(this.pain);
             this.Xternu.Controls.Add(this.button1);
             this.Xternu.Location = new System.Drawing.Point(8, 193);
             this.Xternu.Name = "Xternu";
             this.Xternu.Size = new System.Drawing.Size(1180, 101);
             this.Xternu.TabIndex = 7;
-            // 
-            // pain
-            // 
+
             this.pain.Location = new System.Drawing.Point(3, 77);
             this.pain.Name = "pain";
             this.pain.Size = new System.Drawing.Size(1093, 20);
             this.pain.TabIndex = 2;
-            // 
-            // button1
-            // 
+
             this.button1.Location = new System.Drawing.Point(1102, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 20);
@@ -365,18 +306,14 @@
             this.button1.Text = "отправить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label2
-            // 
+
             this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
             this.label2.Location = new System.Drawing.Point(8, 795);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(161, 174);
             this.label2.TabIndex = 5;
             this.label2.UseMnemonic = false;
-            // 
-            // HistoryBox
-            // 
+
             this.HistoryBox.BackColor = System.Drawing.SystemColors.MenuText;
             this.HistoryBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.HistoryBox.ForeColor = System.Drawing.SystemColors.Info;
@@ -385,9 +322,7 @@
             this.HistoryBox.Name = "HistoryBox";
             this.HistoryBox.Size = new System.Drawing.Size(697, 938);
             this.HistoryBox.TabIndex = 4;
-            // 
-            // label1
-            // 
+
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
@@ -396,9 +331,7 @@
             this.label1.Size = new System.Drawing.Size(70, 22);
             this.label1.TabIndex = 3;
             this.label1.Text = "History";
-            // 
-            // listBox1
-            // 
+
             this.listBox1.BackColor = System.Drawing.SystemColors.InfoText;
             this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -411,9 +344,7 @@
             this.listBox1.TabIndex = 0;
             this.listBox1.TabStop = false;
             this.listBox1.UseTabStops = false;
-            // 
-            // menuStrip1
-            // 
+
             this.menuStrip1.BackColor = System.Drawing.Color.Green;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.applicationToolStripMenuItem});
@@ -422,9 +353,7 @@
             this.menuStrip1.Size = new System.Drawing.Size(1940, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // applicationToolStripMenuItem
-            // 
+
             this.applicationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toTrayToolStripMenuItem,
             this.exitToolStripMenuItem});
@@ -432,31 +361,23 @@
             this.applicationToolStripMenuItem.Name = "applicationToolStripMenuItem";
             this.applicationToolStripMenuItem.Size = new System.Drawing.Size(105, 20);
             this.applicationToolStripMenuItem.Text = "приложение";
-            // 
-            // toTrayToolStripMenuItem
-            // 
+
             this.toTrayToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("toTrayToolStripMenuItem.Image")));
             this.toTrayToolStripMenuItem.Name = "toTrayToolStripMenuItem";
             this.toTrayToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
             this.toTrayToolStripMenuItem.Text = "to tray";
             this.toTrayToolStripMenuItem.Click += new System.EventHandler(this.toTrayToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
+
             this.exitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("exitToolStripMenuItem.Image")));
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
-            // notifyIcon1
-            // 
+
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "notifyIcon1";
-            // 
-            // contextMenuStrip1
-            // 
+ 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem1,
             this.toolStripSeparator1,
@@ -464,35 +385,25 @@
             this.выйтиToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(136, 76);
-            // 
-            // toolStripMenuItem1
-            // 
+
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(135, 22);
             this.toolStripMenuItem1.Text = "выкл пк";
-            // 
-            // toolStripSeparator1
-            // 
+
             this.toolStripSeparator1.Name = "toolStripSeparator1";
             this.toolStripSeparator1.Size = new System.Drawing.Size(132, 6);
-            // 
-            // развернутьToolStripMenuItem
-            // 
+
             this.развернутьToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("развернутьToolStripMenuItem.Image")));
             this.развернутьToolStripMenuItem.Name = "развернутьToolStripMenuItem";
             this.развернутьToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.развернутьToolStripMenuItem.Text = "развернуть";
             this.развернутьToolStripMenuItem.Click += new System.EventHandler(this.развернутьToolStripMenuItem_Click);
-            // 
-            // выйтиToolStripMenuItem
-            // 
+
             this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
             this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.выйтиToolStripMenuItem.Text = "выйти";
             this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
-            // 
-            // button2
-            // 
+
             this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.ForeColor = System.Drawing.Color.Transparent;
@@ -503,9 +414,7 @@
             this.button2.TabIndex = 2;
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
+
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
             this.button3.Location = new System.Drawing.Point(61, 1015);
@@ -514,9 +423,7 @@
             this.button3.TabIndex = 3;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // Form1
-            // 
+
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGreen;
